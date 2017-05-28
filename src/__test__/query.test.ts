8e95a2a32f8406d5102efc9bec61ed5fc15a0bed
Mr D <danieliozzi@gmail.com>
2017-05-28 19:39:22 +0200

    a bit of testing

--- src/__test__/query.test.ts
+++ src/__test__/query.test.ts
@@ -1,21 +1,35 @@
 import * as assert from "assert";
 import { ArgsQuery } from "../args-query";
 describe("args query", () => {
-    it("works", () => {
+
+    it("confirm exists", () => {
         assert.ok(ArgsQuery(["--xyz"]).getFlag("xyz").exists());
     });
+    
+    it("confirm not exists", () => {
+        assert.ok(!ArgsQuery(["--xyzx"]).getFlag("xyz").exists());
+    });
+
     it("reads as string a list", () => {
         assert.equal(
             ArgsQuery(["--x", "1", "2", "3"]).getFlag("x").toString(),
             "1 2 3"
         )
-         assert.equal(
+
+    })
+    
+    it("default to string empty", () => {
+        assert.equal(
             ArgsQuery([]).getFlag("x").toString(),
             ""
         )
-         assert.equal(
+    })
+    
+    it("use provided defaut string value", () => {
+        assert.equal(
             ArgsQuery([]).getFlag("x").toString("?"),
             "?"
         )
-    })
+    });
+
 });
